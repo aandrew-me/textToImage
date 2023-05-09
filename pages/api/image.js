21935ada9670b8ec7ab041fd51de15ed6ef196ea
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-05-09 13:31:38 +0600

    Improvements

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,6 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
-
+require("dotenv").config();
 
 const configuration = new Configuration({
 	apiKey: process.env.API_KEY,
@@ -9,15 +8,23 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
-  if (req.method == "POST"){
-    let jsonReq = req.body
-    console.log(jsonReq);
-    const response = await openai.createImage(jsonReq);
-    const imageList = response.data.data
-    console.log(response.data.data);
-    res.status(200).json({"list":imageList})
-  }
-  else{
-    res.status(200).json({"Status":"Send post request"})
-  }
+	if (req.method == "POST") {
+		let jsonReq = req.body;
+		console.log(jsonReq);
+		try {
+			const response = await openai.createImage(jsonReq);
+			const imageList = response.data.data;
+			console.log(response.data.data);
+			res.status(200).json({ list: imageList, status: true });
+		} catch (error) {
+			console.log(error.response.data.error.message);
+      let message = error.message
+      if (error.response.data.error.message){
+        message = error.response.data.error.message
+      }
+			res.status(403).json({ message, status: false });
+		}
+	} else {
+		res.status(200).json({ Status: "Send post request" });
+	}
 }
--- pages/index.js
+++ pages/index.js
@@ -3,79 +3,90 @@ import { useRef } from "react";
 export default function Home() {
 	const descriptionBtn = useRef("");
 	const numField = useRef("");
-  const loadingMsg = useRef("")
-  const Images = useRef("")
+	const loadingMsg = useRef("");
+	const Images = useRef("");
 
 	const generate = () => {
 		const description = descriptionBtn.current.value;
 		const num = Number(numField.current.value);
-    console.log("Num is " + num)
+		console.log("Num is " + num);
 
-    if (description && num <= 10 && num >= 1){
-      Images.current.innerHTML = ""
-      loadingMsg.current.textContent = "Processing..."
-      loadingMsg.current.style.color = "white"
+		if (description && num <= 10 && num >= 1) {
+			Images.current.innerHTML = "";
+			loadingMsg.current.textContent = "Processing...";
+			loadingMsg.current.style.color = "white";
 
-      const jsonObject = {
-        prompt: description,
-        n: num,
-        size: "512x512",
-      };
-  
-      fetch("/api/image", {
-        method: "POST",
-        body: JSON.stringify(jsonObject),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        }
-      }).then( res => res.json().then(data => {
-        console.log(data);
-        loadingMsg.current.textContent = ""
+			const jsonObject = {
+				prompt: description,
+				n: num,
+				size: "512x512",
+			};
 
-        data.list.forEach(image => {
-          const element = `<img class="images" src=${image.url}></img>`
-          Images.current.innerHTML += element
-        })
-   
-      }))
+			fetch("/api/image", {
+				method: "POST",
+				body: JSON.stringify(jsonObject),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			}).then((res) =>
+				res.json().then((data) => {
+					console.log(data);
+					if (data.status === true) {
+						loadingMsg.current.textContent = "";
 
-    }
-    else{
-      loadingMsg.current.textContent = "Use correct values"
-      loadingMsg.current.style.color = "Red"
-    }
-
-
+						data.list.forEach((image) => {
+							const element = `<img class="images" src=${image.url}></img>`;
+							Images.current.innerHTML += element;
+						});
+					} else {
+						loadingMsg.current.textContent = data.message;
+						loadingMsg.current.style.color = "Red";
+					}
+				})
+			);
+		} else {
+			loadingMsg.current.textContent = "Use correct values";
+			loadingMsg.current.style.color = "Red";
+		}
 	};
 
 	return (
 		<div>
 			<h1>Text to image generator</h1>
-			<p>Describe your image</p>
-			<input
-				ref={descriptionBtn}
-				type="text"
-				id="description"
-				placeholder="Image description"
-			/>
-			<br />
-			<br />
-			<span>Number of images to generate (Max 10) </span>
-			<input
-				ref={numField}
-				type="number"
-				id="num"
-				min={1}
-				max={10}
-				defaultValue={1}
-			/>
-			<br />
-			<br />
-			<button onClick={generate} id="generate">
-				Generate
-			</button>
-			<br />
-      <p ref={loadingMsg}></p>
+			<div id="box">
+				<p>Describe your image</p>
+				<input
+					ref={descriptionBtn}
+					type="text"
+					id="description"
+					placeholder="Image description"
+					onKeyDown={(e) => {
+						if (e.key == "Enter") {
+							generate();
+						}
+					}}
+				/>
+				<br />
+				<br />
+				<span>Number of images to generate (Max 10) </span>
+				<input
+					ref={numField}
+					type="number"
+					id="num"
+					min={1}
+					max={10}
+					defaultValue={1}
+				/>
+				<br />
+				<br />
+
+				<button onClick={generate} id="generate">
+					Generate
+				</button>
+
+				<p ref={loadingMsg}></p>
+			</div>
+
 			<div ref={Images}></div>
 		</div>
 	);
